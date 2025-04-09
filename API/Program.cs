e46434028b3e391e8d63e51ee28a074b779cc7e9
[firstname lastname] <rohithnani650@outlook.com>
2025-04-09 14:16:21 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,18 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddDbContext<AppDbContext>(opt =>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-  
+app.UseCors(x =>x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
-try{   
+try{ 
     var context = services.GetRequiredService<AppDbContext>();
     await context.Database.MigrateAsync();
     await DbInitialzer.SeedData(context);
